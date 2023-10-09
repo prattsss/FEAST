f53c6ef2c7955a1ddb196abc088371def9d1824c
prattsss <prattushvyas@gmail.com>
2023-10-09 21:48:47 +0530

    CI: added class component

--- src/component/classbased/profile.jsx
+++ src/component/classbased/profile.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 class Profile extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
   render(){
     return(
       <>
       <h1>Class Based Profile</h1>
       <h2>name : {this.props.name}</h2>
+      <h2> count : {this.state.count}</h2>
       </>
     )
   }
