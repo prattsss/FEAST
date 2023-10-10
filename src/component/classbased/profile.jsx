e93ddc54a1ed7211c0edd73c809d3af984063eff
prattsss <prattushvyas@gmail.com>
2023-10-10 23:19:57 +0530

    nothing

--- src/component/classbased/profile.jsx
+++ src/component/classbased/profile.jsx
@@ -4,7 +4,8 @@ class Profile extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      count2: "hey"
     }
   }
   render(){
@@ -13,6 +14,7 @@ class Profile extends React.Component{
       <h1>Class Based Profile</h1>
       <h2>name : {this.props.name}</h2>
       <h2> count : {this.state.count}</h2>
+      <button onClick={()=> this.setState({count:"changed"})}>{this.state.count2}</button>
       </>
     )
   }
